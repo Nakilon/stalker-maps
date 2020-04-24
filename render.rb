5641394ff7ca1b4f6af01274c03d6b1cadb4af53
Victor Maslov <nakilon@gmail.com>
2020-04-25 01:45:37 +0300

    merged maps of health and communities

--- render.rb
+++ render.rb
@@ -23,30 +23,32 @@ require "vips"
 image = Vips::Image.new_from_file ARGV[1]
 communities = File.read("out/config/creatures/game_relations.ltx", encoding: "CP1251").encode("utf-8", "cp1251")[/^communities\s*=\s*(.+)/, 1].split(?,).each_slice(2).map(&:first).map &:strip
 npcs.each do |npc|
-  color = [p,p,p,[255,255,255],p,[0,150,0],p,p,p,p,[255,0,0]][npc["community_index"]]
-  x, _, y = npc["position"]
-  image = image.draw_circle color, fx[x], fy[y], 2, fill: true
-end
-image.write_to_file "rendered/#{ARGV[2]}_communities.png"
-
-image = Vips::Image.new_from_file ARGV[1]
-npcs.map do |npc|
   health = npc["health"] || npc["upd:health"]
   fail npc["id"].to_s unless health
-  color, draw_name =
-     if health <= 0 ; [[255, 96,  0]]
-  elsif health < 1  ; [[255, 255, 0], true]
-  elsif health > 1  ; [[0,   255, 0], (npc["name"] != "esc_novice_attacker3")]
-  else              ; [[255, 255, 255]]
-  end
+  fail unless color = [p,p,p,[255,255,255],p,[0,150,0],p,p,p,p,[255,0,0]][npc["community_index"]]
   x, _, y = npc["position"]
-  image = image.draw_circle color, fx[x], fy[y], 2, fill: true
-  if draw_name
-    text = Vips::Image.text npc["character_name"], width: image.width - fx[x] - 5, dpi: 100, font: "Verdana Bold"
-    image = image.composite2(text.new_from_image([192, 192, 192]).copy(interpretation: :srgb).bandjoin(text), :over, x: fx[x] + 5, y: fy[y] - 6).flatten
+  draw_name = !!npc["story_id"]
+  if health == 0
+    image = image.draw_line color, fx[x] - 3, fy[y] - 3, fx[x] + 3, fy[y] + 3
+    image = image.draw_line color, fx[x] - 3, fy[y] + 3, fx[x] + 3, fy[y] - 3
+  elsif health < 0.5
+    image = image.draw_circle color, fx[x], fy[y], 3
+    draw_name = true
+  elsif health < 2
+    image = image.draw_circle color, fx[x], fy[y], 3, fill: true
+  elsif health == 2
+    image = image.draw_circle color, fx[x], fy[y], 3, fill: true
+    draw_name = true
+  else ; fail
+  end
+  if draw_name && npc["name"] != "esc_novice_attacker3"
+    text = Vips::Image.text (
+      %w{ esc_tutorial_dead_novice esc_factory_prisoner_guard }.include?(npc["name"]) ? npc["name"] : npc["character_name"]
+    ), width: image.width - fx[x] - 6, dpi: 100, font: "Verdana Bold"
+    image = image.composite2(text.new_from_image([192, 192, 192]).copy(interpretation: :srgb).bandjoin(text), :over, x: fx[x] + 6, y: fy[y] - 6).flatten
   end
 end
-image.write_to_file "rendered/#{ARGV[2]}_health.png"
+image.write_to_file "rendered/#{ARGV[2]}_npcs.png"
 
 
 puts "OK"
