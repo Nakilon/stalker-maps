a9477c7c2c054ce7e4514a638841988fbcc8263e
Victor Maslov <nakilon@gmail.com>
2020-05-19 03:26:32 +0300

    mutant class colors

--- render.rb
+++ render.rb
@@ -15,16 +15,16 @@ end
 
 require "vips"
 Image = Struct.new :image do
-  def prepare_text x, y, text, dpi = 100
+  def prepare_text x, y, text, dpi = 100, color = [192, 192, 192]
     text = Vips::Image.text text, width: image.width - x - 7, dpi: dpi, font: "Verdana Bold"
     [
-      text.new_from_image([192, 192, 192]).copy(interpretation: :srgb).bandjoin(text),
+      text.new_from_image(color).copy(interpretation: :srgb).bandjoin(text),
       :over, x: x + 7, y: y - 5
     ]
   end
-  def marker x, y, text, dpi = 100
+  def marker x, y, text, dpi = 100, color = [192, 192, 192]
     text = Vips::Image.text text, width: image.width - x - 7, dpi: dpi, font: "Verdana Bold"
-    text = text.new_from_image([192, 192, 192]).copy(interpretation: :srgb).bandjoin(text)
+    text = text.new_from_image(color).copy(interpretation: :srgb).bandjoin(text)
     [text, :over, x: x - text.width / 2, y: y - 5]
   end
 end
--- render_mutants.rb
+++ render_mutants.rb
@@ -11,6 +11,30 @@ abort if mutants.size < 100
 [%w{ rus Всего: }, %w{ eng Total: }].each do |locale, total|
   image = Image.new Vips::Image.new_from_file ARGV[1]
 
+  color = lambda do |name|
+    hsv_to_rgb = lambda do |h, s, v|
+        vmin = (100 - s) * v / 100
+        a = (v - vmin) * (h % 60) / 60.0
+        vinc = vmin + a
+        vdec = v - a
+        case h / 60
+        when 0 ; [v, vinc, vmin]
+        when 1 ; [vdec, v, vmin]
+        when 2 ; [vmin, v, vinc]
+        when 3 ; [vmin, vdec, v]
+        when 4 ; [vinc, vmin, v]
+        when 5 ; [v, vmin, vdec]
+        end.map{ |i| (2.55 * i).round }
+    end
+    {
+      "dog_weak" => [hsv_to_rgb[20, 50, 90]],
+      "dog_normal" => [hsv_to_rgb[20, 50, 90]],
+      "dog_strong" => [hsv_to_rgb[20, 50, 90]],
+      "boar_weak" => [hsv_to_rgb[150, 50, 70]],
+      "boar_normal" => [hsv_to_rgb[150, 50, 70]],
+    }[name]
+  end
+
   # data
   names = mutants.map do |obj|
     health = obj["health"] || obj["upd:health"]
@@ -26,7 +50,7 @@ abort if mutants.size < 100
     else
       obj["section_name"]
     end
-    [name, *image.marker(fx(x), fy(y), name, 80)]
+    [name, obj["section_name"], *image.marker(fx(x), fy(y), name, 80, *color[obj["section_name"]])]
   end.compact
 
   # legend
@@ -37,9 +61,9 @@ abort if mutants.size < 100
   require "mll"
   image.image = image.image.composite2(*image.prepare_text(x + 10, y, total, 160)).flatten
   y += 20
-  MLL::tally[names.map(&:first)].sort_by(&:last).reverse.each do |m, c|
+  MLL::tally[names.transpose[0,2].transpose].sort_by(&:last).reverse.each do |m, c|
     y += 15
-    image.image = image.image.composite2(*image.prepare_text(x + 10, y, "#{c} x #{m}", 80)).flatten
+    image.image = image.image.composite2(*image.prepare_text(x + 10, y, "#{c} x #{m[0]}", 80, *color[m[1]])).flatten
   end
 
   groups = []
@@ -49,8 +73,8 @@ abort if mutants.size < 100
     queue.push names.shift
     until queue.empty?
       group.push current = queue.shift
-      n1, t1, _, xy1 = *current
-      a, b = names.partition do |n2, t2, _, xy2|
+      n1, _, t1, _, xy1 = *current
+      a, b = names.partition do |n2, _, t2, _, xy2|
         n1 == n2 && (xy1[:y] + t1.height > xy2[:y]) &&
                     (xy1[:y] < t2.height + xy2[:y]) &&
                     (xy1[:x] + t1.width > xy2[:x]) &&
@@ -63,11 +87,11 @@ abort if mutants.size < 100
     group = []
   end
   names = groups.map do |group|
-    next group.first.drop 1 if group.size == 1
+    next group.first.drop 2 if group.size == 1
     # TODO: maybe somehow do not init Vips object until here
     x = group.map(&:last).sum{ |_| _[:x] } / group.size
     y = group.map(&:last).sum{ |_| _[:y] } / group.size
-    text, mode, xy = *image.marker(0, 0, "#{group.size}x #{group.first.first}", 80)
+    text, mode, xy = *image.marker(0, 0, "#{group.size}x #{group.first[0]}", 80, *color[group.first[1]])
     [text, mode, x: x, y: y]
   end
   puts "#{groups.sum &:size} => #{names.size}"
