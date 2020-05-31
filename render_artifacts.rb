b8d62718daf8e401d6aeba66b609eb99f6caf9f7
Victor Maslov <nakilon@gmail.com>
2020-05-31 15:05:09 +0300

    refactoring because we need to merge part before the locale loop and the part inside

--- render_artifacts.rb
+++ render_artifacts.rb
@@ -33,34 +33,35 @@ abort "< #{Fixtures.fetch(ARGV[1])[:ARTIFACTS]}" if objs.size < Fixtures.fetch(A
 [%w{ rus в Всего: секрет не }, %w{ eng in\ a Total: secret not\ a }].each do |locale, word, total, stash, not_a|
   image = Render.prepare_image
 
+  localize = lambda do |name|
+    YAML.load Nokogiri::XML(File.read "out/config/text/#{locale}/string_table_enc_zone.xml").at_css("##{
+      File.read("out/config/misc/artefacts.ltx", encoding: "CP1251").encode("utf-8", "cp1251").scan(/^\[([^\]]+).+\n(?:(?:[^\[\n].*)?\n)*inv_name\s*=\s*(\S+)/).to_h.fetch name
+    }").text.strip
+  end
   # data
   names = objs.map do |obj|
     x, _, y = obj["position"]
     require "nokogiri"
-    localize = lambda do |name|
-      YAML.load Nokogiri::XML(File.read "out/config/text/#{locale}/string_table_enc_zone.xml").at_css("##{
-        File.read("out/config/misc/artefacts.ltx", encoding: "CP1251").encode("utf-8", "cp1251").scan(/^\[([^\]]+).+\n(?:(?:[^\[\n].*)?\n)*inv_name\s*=\s*(\S+)/).to_h.fetch name
-      }").text.strip
-    end
-    name_p, size, color = if obj["section_name"][/\Aaf_/]
+    name_s, name_p, size, color = if obj["section_name"][/\Aaf_/]
       image.image = image.image.draw_circle [255, 255, 255], fx(x), fy(y), 2, fill: true
-      [(name_s = localize[obj["section_name"]]), 95]
+      [obj["section_name"], ->_{_}, 95]
     elsif treasures.include? obj["story_id"]
       image.image = image.image.draw_circle [255, 255, 255], fx(x), fy(y), 2
-      ["#{(name_s = treasures[obj["story_id"]][1].map &localize).join ", "} (\"#{
+      [treasures[obj["story_id"]][1], ->_{ "#{_.join ", "} (\"#{
         Nokogiri::XML(File.read "out/config/text/#{locale}/stable_treasure_manager.xml").at_css("##{treasures[obj["story_id"]][0]}").text.strip
       }\")".tap do |s|
         s.gsub!(/(\b\S.{25}\S*) (?=....)/, "\\1\n") if s.size >= 50
-      end, 70, [[160, 160, 160]]]
+      end }, 70, [[160, 160, 160]]]
     else
       image.image = image.image.draw_circle [255, 255, 255], fx(x), fy(y), 2, fill: true
-      [case obj["custom_data"].size
-      when 1 ; "#{name_s = localize[obj["custom_data"][0][1][1][/\S+$/]]} (#{word} #{obj["custom_data"][0][1][0][/\S+$/]})"
-      when 2 ; "#{name_s = localize["af_cristall_flower"]} (#{word} #{obj["section_name"]})"
+      [*case obj["custom_data"].size
+      when 1 ; [obj["custom_data"][0][1][1][/\S+$/], ->_{ "#{_} (#{word} #{obj["custom_data"][0][1][0][/\S+$/]})" } ]
+      when 2 ; ["af_cristall_flower",                ->_{ "#{_} (#{word} #{obj["section_name"]})" }                 ]
       else ; fail
       end, 85]
     end
-    [name_s, name_p, *image.prepare_text(fx(x), fy(y), name_p, size, *color)]
+    name_s = name_s.is_a?(String) ? localize[name_s] : name_s.map(&localize)
+    [name_s, name_p[name_s], *image.prepare_text(fx(x), fy(y), name_p[name_s], size, *color)]
   end
 
   # legend
@@ -75,11 +76,11 @@ abort "< #{Fixtures.fetch(ARGV[1])[:ARTIFACTS]}" if objs.size < Fixtures.fetch(A
     y += 15
     image.image = image.image.composite2(*image.prepare_text(x, y, "#{c} x #{m}", 80)).flatten
   end
-  y += 20
-  image.image = image.image.draw_circle [255, 255, 255], x + 10, y + 3, 3
+  y += 23
+  image.image = image.image.draw_circle [255, 255, 255], x + 10, y, 3
   image.image = image.image.composite2(*image.prepare_text(x + 20, y, "#{stash}")).flatten
   y += 15
-  image.image = image.image.draw_circle [255, 255, 255], x + 10, y + 3, 3, fill: true
+  image.image = image.image.draw_circle [255, 255, 255], x + 10, y, 3, fill: true
   image.image = image.image.composite2(*image.prepare_text(x + 20, y, "#{not_a} #{stash}")).flatten
 
   begin
